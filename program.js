618eea1ed02acdcac22a2957dc5769d1c443596e
cmaycumber <chad.maycumber11@gmail.com>
2018-04-09 11:55:54 +0000

    This is a new program.js commit

---
+++ program.js
@@ -0,0 +1,6 @@
+var express = require('express')
+var app = express()
+
+app.put('/message/:id', function(req, res){
+     res.send(require('crypto').createHash('sha1').update(new Date().toDateString() + req.params.id));
+})
